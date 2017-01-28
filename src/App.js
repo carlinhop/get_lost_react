e01e7289b3e2d91daadee0754f03a53c29a8b1f5
Carlos Pereira <carlinhop@gmail.com>
2017-01-28 18:33:24 +0000

    Preparing for branching and create react routes

--- src/App.js
+++ src/App.js
@@ -14,27 +14,28 @@ class App extends Component {
     constructor(props){
 
         super(props);
-
         this.state = {name: "", google: null, mapWrap: null};
 
-        this.state = Tools.xmlHttp("https://restcountries.eu/rest/v1/all", "GET")
-            .then((results)=>{
-            this.setState({countries: results});
-
-        }).then(()=>{
-            this.setState({city: new City(Tools.getRandomCity(this.state.countries))});
-
-        }).then(()=>{
-            console.log(this.state.city);
-            let mapHTML= document.getElementById("map");
-            let mapWrap = new MapWrapper(mapHTML, {zoom: 4, center: {lat: this.state.city.coordinates[0],
-                    lng:this.state.city.coordinates[1]}});
-            });
 
 
     }
 
     componentDidMount(){
+
+
+        this.state = Tools.xmlHttp("https://restcountries.eu/rest/v1/all", "GET")
+            .then((results)=>{
+                this.setState({countries: results});
+
+            }).then(()=>{
+                this.setState({city: new City(Tools.getRandomCity(this.state.countries))});
+
+            }).then(()=>{
+                console.log(this.state.city);
+                let mapHTML= document.getElementById("map");
+                let mapWrap = new MapWrapper(mapHTML, {zoom: 4, center: {lat: this.state.city.coordinates[0],
+                    lng:this.state.city.coordinates[1]}});
+            });
 
 
     }
@@ -51,12 +52,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-          <div id="map" className="map"></div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="container">
+            <div id="map" className="map"></div>
 
-
+          </div>
       </div>
     );
   }
