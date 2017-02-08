e9e15f34a14ef01a2b16da657793c9b2600e143f
Carlos Pereira <carlinhop@gmail.com>
2017-02-08 17:14:13 +0000

    Got markers appearing and disappearing as they should

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Tools from './models/Tools';
@@ -8,113 +8,122 @@ import MapWrapper from "./models/MapWrapper";
 import SideBar from "./SideBar";
 
 
-
-
 class App extends Component {
 
-    constructor(props){
+    constructor(props) {
 
         super(props);
         this.state = {};
 
 
-
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
         let mapWrap;
 
-         Tools.xmlHttp("https://restcountries.eu/rest/v1/all", "GET")
-            .then((results)=>{
+        Tools.xmlHttp("https://restcountries.eu/rest/v1/all", "GET")
+            .then((results) => {
                 this.setState({countries: results});
 
-            }).then(()=>{
-                this.setState({city: new City(Tools.getRandomCity(this.state.countries))});
+            }).then(() => {
+            this.setState({city: new City(Tools.getRandomCity(this.state.countries))});
 
-            }).then(()=>{
+        }).then(() => {
 
-                let mapHTML= document.getElementById("map");
-                this.setState({mapWrap: new MapWrapper(mapHTML, {zoom: 4, center: {lat: this.state.city.coordinates[0],
-                    lng:this.state.city.coordinates[1]}, disableDefaultUI: true})});
+            let mapHTML = document.getElementById("map");
+            this.setState({
+                mapWrap: new MapWrapper(mapHTML, {
+                    zoom: 4, center: {
+                        lat: this.state.city.coordinates[0],
+                        lng: this.state.city.coordinates[1]
+                    }, disableDefaultUI: true
+                })
             });
-
-
-
-
-    }
-
-    componentWillUpdate(){
-
+        });
 
 
     }
 
-    anotherCity(){
+    componentWillUpdate() {
 
-        //Making the form disappear
+
+    }
+
+    anotherCity() {
+
+        this.state.mapWrap.clearAllMarkers();
+
+
+        //Making the form disappear and clearing it
         let search = document.querySelector(".search");
+        search[0].value = "";
 
-        if( search.style.display = "block"){
+        if (search.style.display = "block") {
             search.style.display = "none";
         }
 
         this.state.mapWrap.map.setZoom(4);
-            this.setState({city: new City(Tools.getRandomCity(this.state.countries))
-            }, ()=>{
+        this.setState({
+            city: new City(Tools.getRandomCity(this.state.countries))
+        }, () => {
 
 
-                this.state.mapWrap.map.setCenter({lat: this.state.city.coordinates[0],
-                    lng:this.state.city.coordinates[1]})
-            });
+            this.state.mapWrap.map.setCenter({
+                lat: this.state.city.coordinates[0],
+                lng: this.state.city.coordinates[1]
+            })
+        });
 
     }
 
-    likeCity(){
+    likeCity() {
+
+
         this.state.mapWrap.centerMap("test", this.state.city);
+
+
         let search = document.querySelector(".search");
         search.style.display = "block";
     }
 
-    searchTerm(event){
+    searchTerm(event) {
         let input = document.querySelector(".search")[0].value;
         event.preventDefault();
 
 
-        this.state.mapWrap.searchMap(input, this.state.city);
+        this.state.mapWrap.searchMap(input, this.state.coordinates);
     }
 
 
-
-  render() {
-
+    render() {
 
 
-    return (
+        return (
 
 
 
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-          <div className="main">
-              <div className="map-container">
-                <div id="map" className="map"></div>
-                  <form className="search" onSubmit={this.searchTerm.bind(this)}>
-                      <input  type="text" placeholder="Search for something"/>
-                  </form>
-                <button onClick={this.anotherCity.bind(this)} className="anotherCity">Another</button>
-                <button onClick={this.likeCity.bind(this)} className="likeCity">Like It</button>
-              </div>
-              <SideBar city={this.state.city}/>
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <div className="main">
+                    <div className="map-container">
+                        <div id="map" className="map"></div>
+                        <form className="search" onSubmit={this.searchTerm.bind(this)}>
+                            <input type="text" placeholder="Search for something"/>
+                        </form>
+                        <button onClick={this.anotherCity.bind(this)} className="anotherCity">Another</button>
+                        <button onClick={this.likeCity.bind(this)} className="likeCity">Like It</button>
+                    </div>
+                    <SideBar city={this.state.city}/>
 
 
-          </div>
-      </div>
-    );
-  }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/models/MapWrapper.js
+++ src/models/MapWrapper.js
@@ -6,16 +6,17 @@ import GoogleMapsLoader from "google-maps";
 GoogleMapsLoader.LIBRARIES = ["geometry", "places"];
 GoogleMapsLoader.KEY = "AIzaSyCc3GjnrXBW2p637XJUP6wbPR8LoqXkaFo";
 
-class MapWrapper{
+class MapWrapper {
 
-    constructor( el, options){
+    constructor(el, options) {
 
         this.map = {};
         this.markers = [];
         this.google = null;
+        this.newCoords = null;
 
 
-        GoogleMapsLoader.load(((google)=>{
+        GoogleMapsLoader.load(((google) => {
 
                 this.google = google;
                 this.map = new google.maps.Map(el, options);
@@ -24,70 +25,86 @@ class MapWrapper{
         );
 
 
-
     }
 
 
-
-    createMap(){
+    createMap() {
         this.map = null;
     }
 
-    createMarker(){
+    deleteMarker(marker) {
 
     }
 
-    AddMarkers(marker){
-        this.markers.push(marker);
+    clearAllMarkers() {
+        for(let marker of this.markers){
+            marker.setMap(null);
+        }
     }
 
-    deleteMarker(marker){
-
-    }
-
-    clearAllMarkers(){
-        this.markers =[];
-    }
-
-    centerMap(term,city){
+    centerMap(term, city) {
 
         let request = {
-            location:
-                new this.google.maps.LatLng(city.coordinates[0],city.coordinates[1]),
+            location: new this.google.maps.LatLng(city.coordinates[0], city.coordinates[1]),
             radius: "1000",
             query: city.name
         };
 
 
         let service = new this.google.maps.places.PlacesService(this.map);
-        console.log(service);
-        service.textSearch(request, (results, status)=>{
+
+        service.textSearch(request, (results, status) => {
             this.map.setCenter(results[0].geometry.location);
             this.map.setZoom(10);
+
+             this.newCoords = results[0].geometry.location;
+
+        });
+
+
+    }
+
+    searchMap(term) {
+
+        //this.map.clearMarkers();
+        let request = {
+            location: this.newCoords,
+            radius: "600",
+            query: term
+        };
+        let service = new this.google.maps.places.PlacesService(this.map);
+
+        service.textSearch(request, (results, status) => {
+            for(let place of results){
+                this.addMarkers(place);
+            }
+
         });
     }
 
-    searchMap(term, city){
-
-            //this.map.clearMarkers();
-
-
-
-            let request = {
-                location:
-                    new this.google.maps.LatLng(city.coordinates[0],city.coordinates[1]),
-                radius: "600",
-                query: term
-
-            };
-            
-
-            let service = new this.google.maps.places.PlacesService(this.map);
-
-        service.textSearch(request, (results, status)=>{
-            console.log(results);
-
+    addMarkers(place){
+        let marker = new this.google.maps.Marker({
+            position: {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()},
+            map: this.map
         });
+        this.markers.push(marker);
+
+        let infowindow = new this.google.maps.InfoWindow({content: place.name});
+        marker.addListener('mouseover', function() {
+            infowindow.open(this.map, marker);
+        });
+
+        marker.addListener('mouseout',function(){
+            infowindow.close();
+        });
+
+        // marker.addListener('click', function(event){
+        //     if(!this.selectedPlaces.includes(place)){
+        //         this.selectedPlaces.push(place);
+        //         this.addPlace(place);
+        //     }
+        //     console.log(this.selectedPlaces);
+
     }
 }
 
